4e03583e85b532d805622c32e596556f600b82d2
Zijun Ye <zijunye@ZijundeMacBook-Pro.local>
2022-02-27 17:13:25 -0800

    modified KlineDataServiceImplTest and TransformationServiceTest

--- src/test/java/com/example/Cryptocurrency/service/KlineDataServiceImplTest.java
+++ src/test/java/com/example/Cryptocurrency/service/KlineDataServiceImplTest.java
@@ -4,16 +4,29 @@ import com.example.Cryptocurrency.model.KlineData;
 import com.example.Cryptocurrency.model.KlineDataRaw;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class KlineDataServiceImplTest {
-    /*
+
+    //todo 补齐剩下的
     @Test
     public void shouldCreateKlineRaw(){
+        KlineDataServiceImpl data = new KlineDataServiceImpl();
+        String[] testString = {"1","2","3","4","5","6","7","8","9","10","11","12"};
+        KlineDataRaw dataRaw = data.create(testString);
+        assertEquals(dataRaw.getOpenTime(),1L);
+        assertEquals(dataRaw.getLowPrice(),"4");
+        assertEquals(dataRaw.getQuoteAssetVolume(),"8");
+        assertEquals(dataRaw.getIgnore(),"12");
 
     }
 
-     */
+
 
 }
--- src/test/java/com/example/Cryptocurrency/service/TransformationServiceTest.java
+++ src/test/java/com/example/Cryptocurrency/service/TransformationServiceTest.java
@@ -1,18 +1,47 @@
 package com.example.Cryptocurrency.service;
 
+import com.example.Cryptocurrency.model.KlineData;
 import com.example.Cryptocurrency.model.KlineDataRaw;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.text.Format;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TransformationServiceTest {
-/*
+    @Autowired
+    TransformationService TransformData;
+
+
     @Test
+    @BeforeEach
     public void shouldTransformation(){
+        KlineDataRaw rawData = new KlineDataRaw();
+        rawData.setOpenTime(Long.parseLong("1523577600000"));
+        rawData.setOpenPrice("2");
+        rawData.setHighPrice("3");
+        rawData.setLowPrice("4");
+        rawData.setClosePrice("5");
+        rawData.setVolume("6");
+        rawData.setCloseTime(Long.parseLong("1523577600000"));
+        rawData.setUuid(UUID.randomUUID());
+        KlineData klineData = TransformData.transformation(rawData);
+        assertEquals(klineData.getClosePrice(), 5);
+        assertEquals(klineData.getVolume(), 6);
+
+
+
 
     }
 
- */
+
 
 }
